ef6a1493b6505f943b45d7894e421e740240fd9d
Lyline <greg.quesne@gmail.com>
2020-06-23 17:01:47 +0200

    Class Projet.java : Create interactive menu

--- src/fr/GCQuesne/Projet.java
+++ src/fr/GCQuesne/Projet.java
@@ -1,5 +1,6 @@
 package fr.GCQuesne;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -9,38 +10,69 @@ import java.util.Scanner;
  * @version 1.0
  */
 public class Projet {
-    public static void main(String[] args) {
-        long accountNumber;
-        char accountType, paymentTheme, paymentType;
-        double accountValue, date;
+  static long accountNumber;
+  static char accountType, paymentTheme, paymentType;
+  static double accountValue, accountSavingRate, date;
+  static int selectionMenu = 0;
 
-        Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    do {
+      System.out.println("1 Créer un compte\n" +
+          "2 Afficher un compte\n" +
+          "3 Crée une ligne comptabe\n" +
+          "4 Sortir\n" +
+          "5 De l'aide\n" +
+          "Votre choix :");
+      selectionMenu = sc.nextInt();
 
-        System.out.println("1 Créer un compte\n" +
-                "2 Afficher un compte\n" +
-                "3 Crée une ligne comptabe\n" +
-                "4 Sortir\n" +
-                "5 De l'aide\n" +
-                "Votre choix :");
+      switch (selectionMenu) {
+        case 1:
+          System.out.println("Type du compte [Type possible : courant, joint, épargne] :");
+          accountType = sc.next().charAt(0);
+          System.out.println("Numéro de compte :");
+          accountNumber = sc.nextLong();
+          System.out.println("Première valeur créditée :");
+          accountValue = sc.useLocale(Locale.US).nextDouble();
+          if (accountType == 'e' || accountType == 'é' || accountType == 'E') {
+            System.out.println("Taux de placement :");
+            accountSavingRate = sc.useLocale(Locale.US).nextDouble();
+          }
+          break;
 
-        int selectionMenu = sc.nextInt();
+        case 2:
+          System.out.println("Saisir le numéro de compte :");
+          long accountNumberCheck = sc.nextLong();
+          if (accountNumberCheck == accountNumber) {
+            System.out.println("Numéro de compte : " + accountNumber
+                + "\nType de compte : " + accountType
+                + "\nValeur initiale : " + accountValue + " €");
+            if (accountType == 'e' || accountType == 'é' || accountType == 'E') {
+              System.out.println("Taux épargne : " + accountSavingRate + " %");
+            }
+          } else System.out.println("Numéro de compte non valide");
+          break;
 
-        System.out.println("Type du compte [Type possible : courant, joint, épargne] :\n" +
-                "Numéro de compte :\n" +
-                "Première valeur créditée :\n" +
-                "Taux de placement :\n");
+        case 3:
+          System.out.println("Option non programmée");
+          break;
 
-        System.out.println("Saisir le numéro de compte :");
-        long accountNumberCheck = sc.nextLong();
+        case 4:
+          System.out.println("Au revoir");
+          System.exit(0);
+          break;
+        case 5:
+          System.out.println("Créer un compte : Ouverture d'un nouveau compte\n" +
+              "Afficher un compte : Consultation d'un compte existant\n" +
+              "Créer une ligne comptable : Ajout d'un mouvement bancaire sur un compte\n" +
+              "Sortir : Fermeture l'application\n" +
+              "De l'aide : Affichage d'un menu d'aide");
+          break;
+        default:
+          System.out.println("Sélection non reconnue");
+      }
+    } while (selectionMenu != 4);
 
-        System.out.println("Option non programmée");
 
-        System.out.println("System.exit(0)");
-
-        System.out.println("Créer un compte : Ouverture d'un nouveau compte\n" +
-                "Afficher un compte : Consulter un compte existant\n" +
-                "Créer une ligne comptable : Ajoute un mouvement bancaire sur un compte\n" +
-                "Sortir : Fermer l'application\n" +
-                "De l'aide : Affiche un menu d'aide");
-    }
+  }
 }
