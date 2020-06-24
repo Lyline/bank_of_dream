bc7fa082d9504d5548da0d15ef1a97ef73fc65f1
Lyline <greg.quesne@gmail.com>
2020-06-24 12:52:57 +0200

    Projet.java : create fonctions -> mainMenu(), exitApplication(), helpApplication()
    mainMenu(): Print the main menu
    exitApplication(): Exit of application
    helpApplication(): Print the help for the main's items

--- src/fr/GCQuesne/Projet.java
+++ src/fr/GCQuesne/Projet.java
@@ -18,13 +18,7 @@ public class Projet {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     do {
-      System.out.println("1 Créer un compte\n" +
-          "2 Afficher un compte\n" +
-          "3 Crée une ligne comptabe\n" +
-          "4 Sortir\n" +
-          "5 De l'aide\n" +
-          "Votre choix :");
-      selectionMenu = sc.nextInt();
+      mainMenu();
 
       switch (selectionMenu) {
         case 1:
@@ -52,27 +46,44 @@ public class Projet {
             }
           } else System.out.println("Numéro de compte non valide");
           break;
-
         case 3:
           System.out.println("Option non programmée");
           break;
-
         case 4:
-          System.out.println("Au revoir");
-          System.exit(0);
+          exitApplication();
           break;
         case 5:
-          System.out.println("Créer un compte : Ouverture d'un nouveau compte\n" +
-              "Afficher un compte : Consultation d'un compte existant\n" +
-              "Créer une ligne comptable : Ajout d'un mouvement bancaire sur un compte\n" +
-              "Sortir : Fermeture l'application\n" +
-              "De l'aide : Affichage d'un menu d'aide");
+          helpApplication();
           break;
         default:
           System.out.println("Sélection non reconnue");
       }
     } while (selectionMenu != 4);
+  }
 
+  public static void mainMenu() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("1 Créer un compte\n" +
+        "2 Afficher un compte\n" +
+        "3 Crée une ligne comptabe\n" +
+        "4 Sortir\n" +
+        "5 De l'aide\n\n" +
+        "Votre choix :");
+    selectionMenu = sc.nextInt();
+  }
 
+  public static void exitApplication() {
+    System.out.println("À Bientôt");
+    System.exit(0);
+  }
+
+  public static void helpApplication() {
+    System.out.println("Créer un compte : Ouverture d'un nouveau compte\n" +
+        "Afficher un compte : Consultation d'un compte existant\n" +
+        "Créer une ligne comptable : Ajout d'un mouvement bancaire sur un compte\n" +
+        "Sortir : Fermeture l'application\n" +
+        "De l'aide : Affichage d'un menu d'aide\n");
   }
 }
+
+
